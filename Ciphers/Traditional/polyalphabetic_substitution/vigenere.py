75e67cd47f6fc45f643d05e49e836aaf5f113e36
orkhasnat <orkhasnat@gmail.com>
2023-08-24 11:13:12 +0600

    Vigenere now works Easy peasy

--- Ciphers/Traditional/polyalphabetic_substitution/vigenere.py
+++ Ciphers/Traditional/polyalphabetic_substitution/vigenere.py
@@ -3,16 +3,26 @@ from itertools import cycle
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 ALPHABET_SIZE = len(ALPHABET)
 
+normalise = lambda ch: ord(ch) - ord(ALPHABET[0])
+denormalise = lambda x: chr(x + ord(ALPHABET[0]))
+
 
 def encode(inp: str, key: int) -> str:
-  res = ""
   iter = cycle(key)
-  for i in inp:
-    res += chr(ord(i) - ord(ALPHABET[0]) + ord(iter))
+  return "".join(
+      map(
+          lambda x: denormalise(
+              (normalise(x) + normalise(next(iter))) % ALPHABET_SIZE)
+          if x in ALPHABET else x, inp))
 
 
 def decode(inp: str, key: int) -> str:
-  pass
+  iter = cycle(key)
+  return "".join(
+      map(
+          lambda x: denormalise(
+              (normalise(x) - normalise(next(iter))) % ALPHABET_SIZE)
+          if x in ALPHABET else x, inp))
 
 
 inp = input()
